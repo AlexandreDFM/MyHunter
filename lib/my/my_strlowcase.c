18c8c0f2effda9d247b98d41b45a62a80424ff1f
AlexandreDFM <alexandre-kevin.de-freitas-martins@epitech.eu>
2021-12-02 17:40:10 +0100

    Fix Coding Style

--- lib/my/my_strlowcase.c
+++ lib/my/my_strlowcase.c
@@ -11,9 +11,9 @@ char *my_strlowcase(char *str)
 {
     int i = 0;
     while (str[i] != 0) {
-	if (str[i] <= 90 && str[i] >= 65) {
+    if (str[i] <= 90 && str[i] >= 65) {
             str[i] = str[i] + 32;
-	}
+    }
         i += 1;
     }
     return (str);
--- lib/my/my_strncat.c
+++ lib/my/my_strncat.c
@@ -12,8 +12,8 @@ char *my_strncat(char *dest, char const *src, int nb)
     int i = 0;
     int counter = my_strlen(dest);
     while (i < nb && src[i] != 0) {
-	dest[counter + i] = src[i];
-	i += 1;
+        dest[counter + i] = src[i];
+        i += 1;
     }
     dest[counter + i] = 0;
     return (dest);
